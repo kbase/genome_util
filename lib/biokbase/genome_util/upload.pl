6e9643e0a9406fe062a361d881963e656bc21d65
pranjan77 <pranjan77@gmail.com>
2015-09-17 16:12:28 -0400

    Update upload.pl

--- lib/biokbase/genome_util/upload.pl
+++ lib/biokbase/genome_util/upload.pl
@@ -3,24 +3,25 @@ use Data::Dumper;
 use JSON;
 use Bio::KBase::workspace::Client;
 use Bio::KBase::AuthToken;
-my $ws_id="pranjan77:1436286867421";
-my $ws_url = "https://ci.kbase.us/services/ws";
 my $token  = $ENV{KB_AUTH_TOKEN};
 if (!$token){
-my $to = Bio::KBase::AuthToken->new();
-$token = $to->{token};
+  my $to = Bio::KBase::AuthToken->new();
+  $token = $to->{token};
 }
+
+#usage perl upload.pl blast.json objname wsname ws_url
+
 my $file = $ARGV[0];
 my $id = $ARGV[1];
-open (FILE, $file) or die ("nnc");
+my $ws_id=$ARGV[2];
+my $ws_url = $ARGV[3];
 
+
+
+open (FILE, $file) or die ("nnc");
 my $ws_doc1 = from_json(join ("", <FILE>));
+close (FILE);
 
 my $ws_doc=$ws_doc1;
-
 my $wsc = Bio::KBase::workspace::Client->new($ws_url, token=>$token );
-
-
-
-
-my $metadata = $wsc->save_object({id =>$id, type =>"KBaseGwasData.BlastOutput", data => $ws_doc, workspace => $ws_id});
+my $metadata = $wsc->save_object({id =>$id, type =>"GenomeUtil.BlastOutput", data => $ws_doc, workspace => $ws_id});
