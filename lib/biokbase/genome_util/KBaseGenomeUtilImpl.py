06b822a0f4e3f71683d36c4c1560ea5d7544d749
Shinjae Yoo <sjyoo@bnl.gov>
2015-09-28 19:34:20 +0000

    Fixed output TO type

--- lib/biokbase/genome_util/KBaseGenomeUtilImpl.py
+++ lib/biokbase/genome_util/KBaseGenomeUtilImpl.py
@@ -363,7 +363,7 @@ class KBaseGenomeUtil:
         res= ws_client.save_objects(
             {"workspace":params['ws_id'],
             "objects": [{
-                "type":"GenomeUtil.BlastOutput",
+                "type":"KBaseGenomeUtil.BlastOutput",
                 "data":res1,
                 "name":params['output_name']}
             ]})
