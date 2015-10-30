d6923e5852ab3d9c05c9577dc635006658ad256e
Shinjae Yoo <sjyoo@bnl.gov>
2015-10-30 19:43:15 +0000

    Fixed syntax error

--- lib/biokbase/genome_util/KBaseGenomeUtilImpl.py
+++ lib/biokbase/genome_util/KBaseGenomeUtilImpl.py
@@ -619,8 +619,8 @@ class KBaseGenomeUtil:
 
            if 'target_seqs' in params:
                res1['BlastOutput_db'] = 'OnTheFlyDB'
-           else
-               res1['BlastOutput_db'] = "%s/%s", (params['ws_id'],params['blastindex_name'])
+           else:
+               res1['BlastOutput_db'] = "%s/%s" %(params['ws_id'],params['blastindex_name'])
  
            self.__LOGGER.info( "Finished!!!")
            self.__LOGGER.debug( res1 )
